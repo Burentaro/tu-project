3f40e53db4f6b204159f6fb77f00d1fcf3f629d9
Andres <jandresfloress@gmail.com>
2019-01-22 12:16:48 +0900

    Fixing Materials and folders
    We move the assets to the right folder and we apply the materials to the cannon, also added the script to the main camera

--- TUBaseProject01/Assets/Scripts/controllers/SpectatorCameraController.cs
+++ TUBaseProject01/Assets/Scripts/controllers/SpectatorCameraController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class SpectatorCameraController : MonoBehaviour {
 	public bool active = true;
 	public float speed = 20;
 
