01d6bb569427e19e8c57e862bb2a6deae2bba2f0
Andres <jandresfloress@gmail.com>
2019-01-22 16:42:27 +0900

    Moving Camera
    Implemented Camera motion

--- TUBaseProject01/Assets/Scripts/controllers/SpectatorCameraController.cs
+++ TUBaseProject01/Assets/Scripts/controllers/SpectatorCameraController.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class SpectatorCameraController : MonoBehaviour {
 	public bool active = true;
-	public float speed = 20;
+	public float speed = 0.5f;
+    public float rotationSpeed = 2;
 
-	private Transform objectsTransform;
+	public Transform objectsTransform;
 	private bool movementActivated = false;
-	private float rightAxis = 0, forwardAxis = 0;
+	private float rightAxis = 0, forwardAxis = 0, upAxis = 0, yaw = 0, pitch = 0;
 
 
 	// Use this for initialization
@@ -22,16 +23,27 @@ public class SpectatorCameraController : MonoBehaviour {
 			// Activate/deactivate movemente with right click
 			if (Input.GetMouseButtonDown (1)) {
 				movementActivated = true;
+                Cursor.visible = false;
 			}
 			if (Input.GetMouseButtonUp (1)) {
 				movementActivated = false;
-			}
+                Cursor.visible = true;
+            }
 
             // Lateral Movement
             rightAxis = Input.GetAxis("Horizontal");
 
             // Frontal movement
             forwardAxis = Input.GetAxis("Vertical");
+
+            // Up and Down movement
+            upAxis = Input.GetAxis("UpDown");
+
+            // Mouse vertical
+            pitch = rotationSpeed * Input.GetAxis("Mouse Y");
+
+            // Mouse horizontal
+            yaw = rotationSpeed * Input.GetAxis("Mouse X");
 
 			// Move
 			if (movementActivated) {
@@ -41,6 +53,15 @@ public class SpectatorCameraController : MonoBehaviour {
 	}
 
 	private void move(){
-		
-	}
+        objectsTransform.Translate(new Vector3(speed * rightAxis, speed * upAxis, speed * forwardAxis));
+        if (pitch > yaw)
+        {
+            objectsTransform.Rotate(-pitch, 0, 0);
+        }
+        else
+        {
+            objectsTransform.Rotate(0, yaw, 0);
+        }
+        
+    }
 }
