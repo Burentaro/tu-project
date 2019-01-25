85f46e52f458a1a6b963e855a93c179d212aff37
anaVirtual <derekpicom@live.com>
2019-01-25 17:58:21 +0900

    Added new scripts for cannonball and powder.

---
+++ TUBaseProject01/Assets/Scripts/Actors/Powder.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Powder : Actor
+{
+    public override void DestroyObject(int delay)
+    {
+
+    }
+}
---
+++ TUBaseProject01/Assets/Scripts/CannonLoader.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CannonLoader : MonoBehaviour
+{
+    public CannonController cannonController;
+    public Actor.ActorType typeToLoad = Actor.ActorType.NONE;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Get the Actor class
+        Actor actor = other.gameObject.GetComponent<Actor>();
+
+        if (actor != null)
+        {
+            if (actor.gameObject.tag == "Ammo" && typeToLoad == Actor.ActorType.AMMO)
+            {
+                // Check to see if the cannon is already loaded so that you don't override the current load
+                if (cannonController.IsCannonBallLoaded)
+                {
+                    // Play Wonk SE
+                    return;
+                }
+
+                // Pass to the cannon the weight and ammo type
+                cannonController.LoadCannonBall(other.gameObject.name);
+                
+                // Desctory the object as it's no longer needed
+                actor.DestroyObject(0);
+            }
+            else if (actor.gameObject.tag == "Powder" && typeToLoad == Actor.ActorType.POWDER)
+            {
+                // Add the powder to the cannon
+                cannonController.AddPowder(actor.weight);
+
+                // Desctory the object as it's no longer needed
+                actor.DestroyObject(0);
+            }
+            else
+            {
+                // Play Wonk SE
+                return;
+            }
+        }
+            
+    }
+}
