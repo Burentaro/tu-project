bc5de1745a7dd8a8f1d3fd4eeb7de15a1e9236b6
Thilo <thilo@asablackbox.com>
2019-01-29 00:45:36 -0800

    SteamVR Input Bindings

---
+++ TUBaseProject01/Assets/Scripts/SteamVRInput.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class SteamVRInput : MonoBehaviour
+{
+    public SteamVR_Action_Boolean upButtonAction;
+    public SteamVR_Action_Boolean downButtonAction;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(SteamVR_Input._default.inActions.ButtonPressDown.GetStateDown(SteamVR_Input_Sources.Any))
+        {
+            Debug.Log("Down Button Pressed");
+        }
+
+        if (SteamVR_Input._default.inActions.ButtonPressUp.GetStateDown(SteamVR_Input_Sources.Any))
+        {
+            Debug.Log("Up Button Pressed");
+         }
+    }
+}
