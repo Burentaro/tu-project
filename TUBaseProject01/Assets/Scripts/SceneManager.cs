024f7299147d6fa103a6aef53519133624ec8fe6
anaVirtual <derekpicom@live.com>
2019-01-29 14:38:39 +0900

    Added scenemanager and singleton classes.

---
+++ TUBaseProject01/Assets/Scripts/SceneManager.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Events;
+
+public class SceneManager : Singleton<SceneManager>
+{
+    [SerializeField]
+    private Transform currentProjectile;        // Holds a reference to the current projectile that has been fired
+    [SerializeField]
+    private float distanceFromCannon;           // Record of the last recorded distance of the cannonball to the cannon
+    [SerializeField]
+    private Transform currentTarget;            // Holds a reference to the current target's location
+
+    [SerializeField]
+    private CannonController cannonController;  // Reference to the CannonController for other objects to be able to access
+    
+    public UnityEvent onTargetHit;              // Event for objects to listen for the event when the projectile hits the target
+    public UnityEvent onTargetMissed;           // Event for objects to listen for the event when the projectile missed the target
+
+
+    private bool hasProjectileFired = false;
+    private bool isActive = false;
+
+    public CannonController CannonController
+    {
+        get
+        {
+            return cannonController;
+        }
+
+        set
+        {
+            cannonController = value;
+        }
+    }
+
+    public float DistanceFromCannon
+    {
+        get
+        {
+            return distanceFromCannon;
+        }
+
+        set
+        {
+            distanceFromCannon = value;
+        }
+    }
+
+    public Transform CurrentProjectile
+    {
+        get
+        {
+            return currentProjectile;
+        }
+
+        set
+        {
+            currentProjectile = value;
+        }
+    }
+
+    public Transform CurrentTarget
+    {
+        get
+        {
+            return currentTarget;
+        }
+
+        set
+        {
+            currentTarget = value;
+        }
+    }
+
+
+    private void Awake()
+    {
+        // Set the isActive flag to true to let the update function run
+        isActive = false;
+    }
+
+    private void Update()
+    {
+        if(isActive)
+        {
+            // Do something
+        }
+    }
+
+    private void OnDestroy()
+    {
+        // Set the isActive flage to false to prevent any errors if SceneManager is destoryed earlier than other classes
+        isActive = false;
+    }
+}
---
+++ TUBaseProject01/Assets/Scripts/Singleton.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T instance;
+    private static object objectLock = new object();
+    private static bool applicationIsQuitting = false;
+
+    public static T Instance
+    {
+        get
+        {
+            if(applicationIsQuitting)
+            {
+                return null;
+            }
+
+            lock (objectLock)
+            {
+                if (instance == null)
+                {
+                    instance = (T)FindObjectOfType(typeof(T));
+                    if(FindObjectsOfType(typeof(T)).Length > 1)
+                    {
+                        return instance;
+                    }
+
+                    if(instance == null)
+                    {
+                        GameObject singleton = new GameObject();
+                        instance = singleton.AddComponent<T>();
+                        singleton.name = "(singleton) " + typeof(T).ToString();
+                    }
+                }
+
+                return instance;
+            }
+        }
+    }
+
+    private void Awake()
+    {
+        applicationIsQuitting = false;
+    }
+
+    private void OnDestroy()
+    {
+        applicationIsQuitting = true;
+    }
+}
