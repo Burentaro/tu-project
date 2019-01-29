5cfb4fad7712d94bf4d35f30589cada4e5bbd0b8
Jose Andres Flores <jandresfloress@gmail.com>
2019-01-29 14:44:08 +0900

    Added RemoteController basic functions
    Added the basic functionalities for the RemoteController

---
+++ TUBaseProject01/Assets/Scripts/controllers/RemoteController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemoteController : MonoBehaviour
+{
+    public CannonController cannonController;
+    public bool isHeld = false;
+
+    // Start is called before the first frame update
+    private bool fire = false;
+    private float upDownAxis = 0;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isHeld)
+        {
+            if (Input.GetKeyDown("Fire1"))
+            {
+                fire = true;
+            }
+            if (Input.GetKeyUp("Fire1"))
+            {
+                fire = false;
+            }
+            upDownAxis = Input.GetAxis("UpDown");
+        }
+    }
+
+    private void actions()
+    {
+        if (fire)
+        {
+            Debug.Log("Shoot");
+        }
+        if(upDownAxis < 0)
+        {
+            Debug.Log("Up?");
+        }
+        if(upDownAxis > 0)
+        {
+            Debug.Log("Down?");
+        }
+    }
+}
