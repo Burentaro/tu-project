5c674a36a5828fcf826e5c4493c58ed57fb659bc
gareau <thilo@asablackbox.com>
2019-01-31 13:36:54 +0900

    Adjustments to Target - Replacement spawns at correct location

--- TUBaseProject01/Assets/Scripts/ColliderTrigger.cs
+++ TUBaseProject01/Assets/Scripts/ColliderTrigger.cs
@@ -9,12 +9,15 @@ public class ColliderTrigger : MonoBehaviour {
     void OnTriggerEnter(Collider other)
     {
         // Check that the object collided with is a button
+        Debug.Log("On TriggerEnter 1");
         if (other.tag == tagName)
         {
             // Check that the UnityEnvent is not null to avoid a null exception
+            Debug.Log("On TriggerEnter 2");
             if (onColliderTriggered != null)
             {
                 // Let all listeners know that the button has been triggered
+                Debug.Log("On TriggerEnter 3");
                 onColliderTriggered.Invoke();
             }
         }
