12e593e158603d1b05c813cb3dc2f7d86e1a960d
anaVirtual <derekpicom@live.com>
2019-02-01 13:26:26 +0900

    Added UI scripts

---
+++ TUBaseProject01/Assets/Scripts/UIBlackboard.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIBlackboard : MonoBehaviour
+{
+    public Text targetDistanceText;
+    public Text shotDistanceText;
+    public Text cannonballText;
+    public Text powderText;
+    public Text angleText;
+
+    public void OnTargetMissed(ShootHistory history)
+    {
+        // Update Text fields
+
+    }
+
+    public void OnTargetHit(ShootHistory history)
+    {
+        // Update Text field;
+    }
+}
---
+++ TUBaseProject01/Assets/Scripts/UICannon.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UICannon : MonoBehaviour
+{
+    public Text cannonballText;
+    public Text powderText;
+    public Text angleText;
+
+    public void OnCannonUpdated(float angle, float powder, string cannonBall)
+    {
+        // Update the UI elements
+    }
+}
